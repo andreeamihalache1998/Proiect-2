57e380cfe07b055a656e68a27dd3640997c481a8
andreeamihalache1998 <62653449+andreeamihalache1998@users.noreply.github.com>
2020-04-08 13:00:43 +0300

    Add files via upload

---
+++ p2.cpp
@@ -0,0 +1,87 @@
+// Pentru o fereastra de dimensiuni 24x24 vom avea aprox. 160 000 de caracteristici (la scala minima)
+// Vom cauta toate caracteristicile pentru toate dimensiunile
+// Pentru fiecare fereastra de 24x24 vom avea aprox. 2500 caracteristici
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Feature {
+    int type; // am declarat tipurile de caracteristici
+    int width, height; // latimea si inaltimea fiecarei caracteristici
+    int xc, yc; // coordonatele x si y ale coltului din stanga-sus ale caracteristicii in subfereastra de detectie
+    float rectangleValue(float* ii, int iiwidth, int ix, int iy, int rx, int ry, int rw, int rh);// ii=imaginea integrala
+public:
+    Feature(int type, int xc, int yc, int width, int height);// constructor
+    float getValue(float* ii, int iiwidth, int x, int y);
+    int getType();
+    int getWidth();
+    int getHeight();
+    void scale(float s);
+    std::string toString();
+};
+
+Feature::Feature(int type, int xc, int yc, int width, int height) { // definim constructorul
+    this->type = type;
+    this->xc = xc;
+    this->yc = yc;
+    this->width = width;
+    this->height = height;
+}
+
+string int_to_string(int i) {// functie de conversie din int in string
+    stringstream ss;
+    ss << i;
+    string str = ss.str();
+    return str;
+}
+
+string Feature::toString() {// concantenam toate valorile string ale parametrilor unei caracteristici
+    string str;
+    str = int_to_string(type) + " " + int_to_string(width) + " " + int_to_string(height) + " " + int_to_string(xc) + " " + int_to_string(yc);
+    return str;
+}
+
+int Feature::getType() { // returnam tipul (pentru ca este declarat privat in clasa)
+    return this->type;
+}
+
+int Feature::getWidth() { // returnam latimea (pentru ca este declarata privat in clasa)
+    return this->width;
+}
+
+int Feature::getHeight() { // returnam inaltimea (pentru ca este declarata privat in clasa)
+    return this->height;
+}
+
+/*float Feature::rectangleValue(float *ii, int iiwidth, int ix, int iy, int rx, int ry, int rw, int rh) {
+  return;
+}*/
+
+/*float Feature::getValue(float *ii, int iiwidth, int x, int y) {
+  if(this->type == 1)
+      return rectangleValue()-rectangleValue();
+  else if(this->type == 2)
+        return rectangleValue()-rectangleValue();
+  else if(this->type == 3)
+      return rectangleValue()-rectangleValue()-rectangleValue();
+  else if(this->type == 4)
+      return;
+      .
+      .
+      .
+   else {
+      cout << "Eroare: caracteristica de tip " << this->type << " nu exista. Tipul caracteristicilor se afla in intervalul [,].\n";
+      exit(-1);
+  }
+}*/
+
+void Feature::scale(float s) {
+    this->width *= s;
+    this->height *= s;
+    this->xc *= s;
+    this->yc *= s;
+}
