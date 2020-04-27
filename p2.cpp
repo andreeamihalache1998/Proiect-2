8873aab10c0589337f6c35112804cf0bd86199fc
burgundicorn <63016038+burgundicorn@users.noreply.github.com>
2020-04-27 10:55:28 +0300

    Update p2.cpp

--- p2.cpp
+++ p2.cpp
@@ -2,18 +2,48 @@
 // Vom cauta toate caracteristicile pentru toate dimensiunile
 // Pentru fiecare fereastra de 24x24 vom avea aprox. 2500 clasificatori slabi
 
-#include<opencv2/opencv.hpp>
 #include<opencv2/core/mat.hpp>
-#include<iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
 #include<algorithm>
 #include<fstream>
 #include<cmath>
 #include <cstdlib>
 
+
+#include <cstdio>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <vector>
+#include <opencv2/imgproc/imgproc.hpp>
+
+
+
 using namespace std;
 using namespace cv;
+
+class pixel{
+	int r;
+	int g;
+	int b;
+public:
+	pixel(int r, int g, int b) {
+		this->r = r;
+		this->g = g;
+		this->b = b;
+	}
+	int getR() {
+		return r;
+	}
+	int getG() {
+		return g;
+	}
+	int getB() {
+		return b;
+	}
+};
+
+
 //Formarea Caracteristicilor
 class Feature {
     int type; // am declarat tipurile de caracteristici
@@ -158,27 +188,109 @@ void WeakClassifier:: train_Classifier() const  {
 }
 //weight =1/2*log((1-error_rate)/error_rate);   pentru antrenare
 
-int main()
-{
 
-	//afisarea imaginii
-	Mat img = imread("E:\\FACULTATE\\lena.png", 1);
-	if (!img.data)
-	{
-		cout << "No image data \n";
-		return -1;
+
+int main() {
+	cout << "Se citeste imaginea.";
+	//cin.get();
+
+	vector<vector<pixel>> image; // declar matricea de pixeli
+
+
+	Mat grey_image;
+
+	Mat read_image; 
+	read_image = imread("Data\\test.png", IMREAD_COLOR);		// se citeste imaginea
+
+	if (!read_image.data) {								// se verifica daca input-ul este valid
+		cout << "Imaginea nu a putut fi incarcata." << endl;
+		exit(0);
 	}
-	//Convertim imaginea in alb-negru- GreyScale
-	Mat img_gray;
-	cvtColor(img, img_gray, COLOR_BGR2GRAY);
-	namedWindow("Display Original Image", WINDOW_AUTOSIZE);
-	imshow("Display Original Image", img);
 
-	//Display the grayscale image 
-	namedWindow("Display Grayscale Image", WINDOW_AUTOSIZE);
-	imshow("Display Grayscale Image", img_gray);
-    waitKey(0);
+	int width = read_image.cols, height = read_image.rows, channelsNo = read_image.channels();
+
+	cout << "Dimensiunile imaginii sunt:\nlatime: " << width << "\ninaltime: " << height << "\nchannels no: " << channelsNo << "\n";
+
+
+	cvtColor(read_image, grey_image, COLOR_BGR2GRAY); // convertire BGR -> greyscale
+
+	/*for (int j = 0; j < height; j++) {
+		vector<pixel> vec;
+		for (int i = 0; i < width*channelsNo; i += channelsNo) {
+			int r, g, b;
+			r = read_image.at<char>(j, i + 2)/255 * 0.2126;
+			g = read_image.at<char>(j, i + 1)/255 * 0.7152;
+			b = read_image.at<char>(j, i + 0)/255 * 0.0722;
+			pixel img_pixel(r, g, b);
+			vec.push_back(img_pixel);
+		}
+		image.push_back(vec);
+		vec.clear();
+	}
+
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width*channelsNo; i += channelsNo) {
+			cout << image.at(j).at(i).getR() << " " << image.at(j).at(i).getG() << " " << image.at(j).at(i).getB() << "; ";
+		}
+		cout << "\n";
+	}*/
+
+
+	/*for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width*channelsNo; i += channelsNo) {
+			//cout << (int)read_image.at<char>(j, i + 2) << " " << (int)read_image.at<char>(j, i + 1) << " " << (int)read_image.at<char>(j, i + 0) << "; ";
+			unsigned char red = read_image.imageData[j*read_image.widthStep + i + 2];
+
+			read_image.at<char>(j, i + 1) *= 0.2152;
+			read_image.at<char>(j, i + 0)  *= 0.2722;
+		}
+		//cout << "\n";
+	}*/
+
+
+
+
+	vector<vector<int>> vec_dragut;
+	for (int i = 0; i< height; i++)
+	{
+		vector<int> linie;
+		for (int j = 0; j< width; j++)
+		{
+			linie.push_back((int)grey_image.at<uchar>(i, j));
+		}
+		vec_dragut.push_back(linie);
+		linie.clear();
+	}
+
+
+	/*for (int i = 0; i < height; i++) { // test afisare valori matrice pixeli greyscale
+		for (int j = 0; j < width; j++) {
+			cout << vec_dragut[i][j] << " ";
+		}
+		cout << "\n";
+	}*/
+
+	//cout << vec_dragut[0][0] << " " << vec_dragut[0][width-1] << " \n" << vec_dragut[height-1][0] << " " << vec_dragut[height-1][width-1]; //test
+
+
+
+
+
+	namedWindow("Afisare imagine", WINDOW_AUTOSIZE); // se creaza o fereastra pentru afisare
+	imshow("Afisare imagine", grey_image);				 // se afiseaza imaginea citita in fereastra de afisare
+	waitKey(0);											// se asteapta apasarea unei taste
+	//read_image.release();
+	//grey_image.release();
+
+	/*Vec3b intensity = image.at<Vec3b>(y, x);
+
+	float blue = intensity.val[0];
+	float green = intensity.val[1];
+	float red = intensity.val[2];
+
+	std::cout << "Blue:" << blue << std::endl;
+	std::cout << "Green:" << green << std::endl;
+	std::cout << "Red:" << red << std::endl;*/
+	//cin.get();
 	return 0;
-
-
 }
